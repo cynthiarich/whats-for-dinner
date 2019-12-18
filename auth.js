289f984bc03c7f147cc9ce6c38b3d4e0802a3ae5
Cynthia Rich <crichID@github.com>
2019-12-17 21:17:36 -0500

    authentication is working

--- auth.js
+++ auth.js
@@ -1,4 +1,5 @@
-const ui = new firebaseui.auth.authUI(firebase.auth());
+//initialize the firebaseUI widget
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 const uiConfig = {
     callbacks: {
@@ -6,15 +7,15 @@ const uiConfig = {
             return true;
         },
         uiShown() {
-            document.getElementById("loader").style.display = "none";
+            document.getElementById('loader').style.display = 'none';
         },
     },
-    signInFlow: "popup",
-    signInSuccessUrl: "signedIn",
+    signInFlow: 'popup',
+    signInSuccessUrl: 'signedIn',
     signInOptions: [
+        //we are providing the option to sign in with email or google
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     ],
 };
-
-ui.start("#firebaseui-auth-container", uiConfig);
+ui.start('#firebaseui-auth-container', uiConfig);
