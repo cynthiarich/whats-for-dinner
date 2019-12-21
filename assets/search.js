08bc749227fd13731f28bfb189490dddad64ffa4
William Vaughn <will.d.vaughn@gmail.com>
2019-12-21 13:10:15 -0500

    enabled search for multiple options got collect full recipes found with multiple searches

--- assets/search.js
+++ assets/search.js
@@ -1,22 +1,16 @@
 //https://api.edamam.com/search?q=chicken&app_id=3e6eb808&app_key=320b9c0c25d10bd3661f72afc26368db&to=2
 var apiIdEdamam = "3e6eb808"; // api id for edamam
 var apikeyEdamam = "320b9c0c25d10bd3661f72afc26368db"; // api key for edamam
-var recipes; // array for the recipie info
+var recipes = []; // array for the recipie info
 
 // loops through the array information received from the user
 function loopArray(data)
 {
     var url = "";
 
-    for (var i = 0; i < data.length; i++)
+    for (var i = 1; i < data.length; i++)
     {
-        url += data[i];
-
-        // adds a + sign after each element of the array other than the final element
-        if (i + 1 < data.length)
-        {
-            url += "+"
-        }
+        url += "&" + data[0] + "=" + data[i];
     }
     
     return url;
@@ -25,41 +19,41 @@ function loopArray(data)
 // search function calling edamam to get recipes
 function search()
 {
-    var queryURL = "https://api.edamam.com/search?";
-    retrieveData(); // calls function to get user input
-
-    queryURL += "q=";
-
-    queryURL += loopArray(interestedFoods); 
-    queryURL += "&app_id=" + apiIdEdamam + "&app_key=" + apikeyEdamam;
-
-    /// adds data to url if there are element of dietOptionsUsed
-    if (dietOptionsUsed.length > 0)
+    for (var i = 0; i < interestedFoods.length; i++)
     {
-        queryURL += "&diet=";
-        queryURL += loopArray(dietOptionsUsed);
-    }
+        var queryURL = "https://api.edamam.com/search?q=";
 
-    /// adds data to url if there are element of healthOptionsUsed
-    if (healthOptionsUsed.length > 0)
-    {
-        queryURL += "&health=";
-        queryURL += loopArray(healthOptionsUsed);
-    }
+        queryURL += interestedFoods[i];
+        queryURL += "&app_id=" + apiIdEdamam + "&app_key=" + apikeyEdamam;
 
-    // gets the first 10 results from edamam
-    queryURL += "&to=10";
-
-    $.ajax(
+        /// adds data to url if there are element of dietOptionsUsed
+        if (dietOptionsUsed.length > 1)
         {
+            queryURL += loopArray(dietOptionsUsed);
+        }
+
+        /// adds health to url if there are element of healthOptionsUsed
+        if (healthOptionsUsed.length > 1)
+        {
+            queryURL += loopArray(healthOptionsUsed);
+        }
+
+        // gets the first 10 results from edamam
+        queryURL += "&to=10";
+        console.log(queryURL);
+        $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response)
         {
+            var hits = response.hits;
+            
+            for (var j = 0; j < hits.length; j++)
+            {
+                recipes.push(hits[j]);
+            }
 
-            hits = response.hits;
-            console.log(hits);
+            console.log(recipes);
         });
+    }
 }
-
-search();
