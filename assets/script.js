5ced31cc60e63d54ffd406adeef70a6c0dc444dd
Brittany Clark <clark.brittany.j@gmail.com>
2020-01-02 18:56:54 -0500

    activity dropdown

--- assets/script.js
+++ assets/script.js
@@ -47,17 +47,28 @@ $(document).on("click", ".pref-btn", function () {
         dietDiv.append($("<label>").text(dietOptions[i]));
     }
 });
+// var li2 = ($("<li>").text("test")); //cant do it here cause it keeps appending to this one
+function createActivityList() { //only appends on first div card regardless, need to add class to button maybe and append uniquely
+    // var li2 = ($("<li>")); 
+    console.log("start");
+    for (i = 0; i < activities.length; i++) {
+        var atag = $("<a>").attr("href", "#").attr("id", "choiceDrop").text(activities[i]); //replace href later with a class to register click event?
+        li2.append(atag);
+    }
+    console.log("end");
 
-function createMenu(){
+}
+
+function createMenu() {
     $("#weekdisplay").empty();
     $("#weekdisplay").append($("<button>").attr("class", "uk-button uk-button-default uk-width-1-2 menu-update").text("Create a new menu"));
-    for (var i = 0; i < days.length; i++){
+    for (var i = 0; i < days.length; i++) {
         var newCard = $("<div>").attr("class", "uk-card uk-card-default uk-width-1-1");
         var cardHead = $("<div>").attr("class", "uk-card-header");
         newCard.append(cardHead);
         var cardTitle = $("<h3>").attr("class", "uk-card-title").text(days[i]);
         cardHead.append(cardTitle);
-        
+
         var cardBody = $("<div>").attr("class", "uk-card-body");
         newCard.append(cardBody);
 
@@ -72,10 +83,29 @@ function createMenu(){
         recipe.append($("<p>").text("This is the recipe for " + days[i]));
         li.append(recipe);
         cardBody.append(ul);
+        console.log("one");
+        //it would be nice to add another line/separator between the menu and activity? like put the dropdown in the footer?
+
         //create drop-down in body of daily menu div
-        cardBody.append($("<button>").attr("id", "activity").text("Hold For Activity Select");
-        
+        //creates button
+        var activeSelect = $("<button>").attr("id", "activity").attr("class", "uk-button uk-button-primary").text("Choose Activity");
+        var selections = $("<div>").attr("uk-dropdown", "");
+        //iterates through activities array to create dropdown
+        var ul2 = ($("<ul>").attr("class", "uk-nav uk-dropdown-nav"));
+        // ul2.append(createActivityList());
+        var li2 = ($("<li>"));
+        var atag = $("<a>").attr("href", "#").attr("id", "choiceDrop").text(activities[i]); //replace href later with a class to register click event?
+        var btag = $("<a>").attr("href", "#").attr("id", "choiceDrop").text(activities[i + 1]);
+        li2.append(atag).append(btag);;
+        ul2.append(li2);
+        selections.append(ul2);
+
+        //appends the button within the div
+        cardBody.append(activeSelect).append(selections); //could append to cardBody OR newCard
+
         $("#weekdisplay").append(newCard);
+        //needs to store the value from click to pass to API
+        //pulls from bored api to return an activity from the dropdown selection
     }
 };
 
@@ -94,6 +124,7 @@ $("#open").on("click", function () {
     console.log(UIkit.modal("#sign-in"));
 });
 
-$(".save-prefs").on("click", retrieveData);
+// $(".save-prefs").on("click", retrieveData);
+$(".save-prefs").on("click", createMenu());
 
-$(".menu-update").on("click", createMenu);
+// $(".menu-update").on("click", Menu);
